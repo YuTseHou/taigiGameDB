e75e8a5fabbc52fb31c9e3ee29562f1f9f6f69cf
YuTseHou <killer40207@gmail.com>
2015-05-06 11:57:37 +0800

    update celery

--- queryClosePronounce/tasks.py
+++ queryClosePronounce/tasks.py
@@ -1,37 +1,7 @@
 from __future__ import absolute_import
 from celery import shared_task
-from django.shortcuts import render
-from django.http import HttpResponse
-from queryClosePronounce.models import Pronounce
 from difflib import SequenceMatcher
 
-
+@shared_task
 def similar(strA, strB):
     return SequenceMatcher(None, strA, strB).ratio()
-
-@shared_task
-def query_task(request, pronounceQ):
-
-    # cut off if pronounceQ is too short
-    if(len(pronounceQ) <= 1):
-        return HttpResponse("Query is too short")
-
-    # the string for returning
-    theAnswer = ""
-
-    allPronounce = Pronounce.objects.all()
-
-    # The rank for all simlarities(pronounce, pronounceQ)
-    # Higher is better
-    theRank = {}
-
-    # singlePronounce contains its pronounce and matching chinese chracters
-    for singlePronounce in allPronounce:
-        theRank[singlePronounce.pronounce] = similar(pronounceQ, singlePronounce.pronounce)
-
-    # sorting ranking and append result to theAnswer
-    for theKey, theSimilarity in sorted(theRank.iteritems(), key = lambda (k, v) : (v, k), reverse=True):
-        if(theSimilarity > 0):
-            theAnswer = theAnswer + theKey + ", " + str(round(theSimilarity, 2)) + " : " + allPronounce.get(pronounce = theKey).chineses + "<br>"
-
-    return theAnswer
--- queryClosePronounce/views.py
+++ queryClosePronounce/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from queryClosePronounce.models import Pronounce
-from difflib import SequenceMatcher
-from queryClosePronounce.tasks import query_task
+from queryClosePronounce.tasks import similar
 
 '''
 def similar(strA, strB):
@@ -14,8 +13,8 @@ def hello_world(request):
 
 
 def query(request, pronounceQ):
-    theAnswer = query_task.delay(request, pronounceQ)
-    '''
+    #theAnswer = query_task.s(request, pronounceQ).apply_async(countdown=10)
+    
     # cut off if pronounceQ is too short
     if(len(pronounceQ) <= 1):
         return HttpResponse("Query is too short")
@@ -31,13 +30,13 @@ def query(request, pronounceQ):
 
     # singlePronounce contains its pronounce and matching chinese chracters
     for singlePronounce in allPronounce:
-        theRank[singlePronounce.pronounce] = similar(pronounceQ, singlePronounce.pronounce)
+        theRank[singlePronounce.pronounce] = similar.delay(pronounceQ, singlePronounce.pronounce)
 
     # sorting ranking and append result to theAnswer
     for theKey, theSimilarity in sorted(theRank.iteritems(), key = lambda (k, v) : (v, k), reverse=True):
         if(theSimilarity > 0):
-            theAnswer = theAnswer + theKey + ", " + str(round(theSimilarity, 2)) + " : " + allPronounce.get(pronounce = theKey).chineses + "<br>"
-    '''
+            theAnswer = "test"#theAnswer + theKey + ", " + str(round(theSimilarity, 2)) + " : " + allPronounce.get(pronounce = theKey).chineses + "<br>"
+    
 
     if(theAnswer == ""):
         return HttpResponse("No results")
