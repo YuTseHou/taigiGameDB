309bb9409bbcb99aad6ce5d6191fb34bd6a67437
YuTseHou <killer40207@gmail.com>
2015-05-10 21:05:44 +0800

    update

--- queryClosePronounce/tasks.py
+++ queryClosePronounce/tasks.py
@@ -3,5 +3,14 @@ from celery import shared_task
 from difflib import SequenceMatcher
 
 @shared_task
-def similar(strA, strB):
-    return SequenceMatcher(None, strA, strB).ratio()
+def matcher(strA, strB):
+	return SequenceMatcher(None, strA, strB).ratio()
+
+@shared_task
+def similar(allPronounce, pronounceQ):
+	theRank = {}
+	for singlePronounce in allPronounce:
+		tmp = matcher(pronounceQ, singlePronounce.pronounce)
+		if(tmp > 0.6):
+			theRank[singlePronounce.pronounce] = tmp
+	return theRank
--- queryClosePronounce/views.py
+++ queryClosePronounce/views.py
@@ -3,18 +3,12 @@ from django.http import HttpResponse
 from queryClosePronounce.models import Pronounce
 from queryClosePronounce.tasks import similar
 
-'''
-def similar(strA, strB):
-	return SequenceMatcher(None, strA, strB).ratio()
-
-'''
 def hello_world(request):
     return HttpResponse("Hello taigiGameDB!")
 
 
 def query(request, pronounceQ):
-    #theAnswer = query_task.s(request, pronounceQ).apply_async(countdown=10)
-    
+
     # cut off if pronounceQ is too short
     if(len(pronounceQ) <= 1):
         return HttpResponse("Query is too short")
@@ -29,17 +23,19 @@ def query(request, pronounceQ):
     theRank = {}
 
     # singlePronounce contains its pronounce and matching chinese chracters
+    '''
     for singlePronounce in allPronounce:
-        theRank[singlePronounce.pronounce] = similar.delay(pronounceQ, singlePronounce.pronounce)
+        tmp = similar.delay(pronounceQ, singlePronounce.pronounce)
+        if(tmp > 0.6):
+            theRank[singlePronounce.pronounce] = tmp
+    '''
+    theRank = similar.delay(allPronounce, pronounceQ)
 
     # sorting ranking and append result to theAnswer
     for theKey, theSimilarity in sorted(theRank.iteritems(), key = lambda (k, v) : (v, k), reverse=True):
-        if(theSimilarity > 0):
-            theAnswer = "test"#theAnswer + theKey + ", " + str(round(theSimilarity, 2)) + " : " + allPronounce.get(pronounce = theKey).chineses + "<br>"
-    
+        theAnswer = theAnswer + theKey + ", " + str(round(theSimilarity, 2)) + " : " + allPronounce.get(pronounce = theKey).chineses + "<br>"
 
     if(theAnswer == ""):
         return HttpResponse("No results")
     else:
         return HttpResponse(theAnswer)
-        
